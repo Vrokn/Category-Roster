4f155d6c476610647d66668b80733b1a2c99c1a3
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-08-18 16:02:50 -0500

    okk

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
-
+import image from '../mediafiles/Rooster.png'
 export default function Header({ onChange }) {
 
     return (
-        <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="/">Movie Roster</Navbar.Brand>
+        <Navbar bg="black" expand="lg" variant="dark">
+            <Navbar.Brand href="/">
+                <img
+                    alt=""
+                    src={image}
+                    height="70"
+                    className="d-inline-block align-center"
+                />
+                <div className='brand'>
+                    MovieRooster
+                    </div>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
                     <NavDropdown title="Discover" id="basic-nav-dropdown">
                         <NavDropdown.Item href="/Popular">Popular</NavDropdown.Item>
                         <NavDropdown.Item href="/Trending">Trending</NavDropdown.Item>
